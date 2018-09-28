0d97688b5d438d7f22a01b266e0d472fd4f3ee5d
Cong <cczzrs@aliyun.com>
2018-09-28 15:20:28 +0800

    配置更新 setting update

--- README.md
+++ README.md
@@ -4,20 +4,34 @@
 爬虫、页面列表数据、页面详情数据、增量监控、爬取规则录入、规则列表、规则管理、并发队列、爬取进度 chart图、登录注册、管理员后台等。
 
 #### 软件架构
-python nginx uwsgi django shiro rest  selenium chrome
+python3 nginx uwsgi django shiro rest selenium chrome lxml
+mysql
 
 
 #### 安装教程
 
-1. 环境 python nginx uwsgi chrome
+1. 环境 python nginx uwsgi python3 django selenium chrome lxml ...
 2. 运行 uwsgi --ini ./pac/my_uwsgi.ini
-3. http://0-0.cc/pac/wiki
+2. 运行 nginx -c ./pac/nginx.conf
+3. 详情查看 http://0-0.cc/pac/wiki
 
 #### 使用说明
 
-1. xxxx
-2. xxxx
-3. xxxx
+1. 环境变量
+    chrome
+    chromedriver
+
+2.数据库配置
+    DATABASES_ENGINE =  'django.db.backends.mysql'  # 数据库的类型
+    DATABASES_NAME =    'pactera'   if this_os else 'pac'              # 所使用的的数据库的名字
+    DATABASES_USER =    'root'      if this_os else 'root'             # 数据库服务器的用户
+    DATABASES_PASSWORD ='root'      if this_os else 'root'             # 密码
+    DATABASES_HOST =    '0-0.cc'    if this_os else 'mysql'            # 主机
+    DATABASES_PORT =    '3306'      if this_os else '3306'             # 端口
+
+3.静态文件 static 集成路径 (nginx static file path)
+    STATIC_ROOT = r'C:/Users/static_data/' if this_os else r'/home/pac/static_data/'
 
 #### find me
 cczzrs@aliyun.com
+http://0-0.cc
--- settings.py
+++ settings.py
@@ -14,15 +14,15 @@ PRINT_LOG = True if this_os else False
 TEST_RUN_COUNT = 3 if this_os else 1000
 
 # 文件生成地址，供下载使用  (前面是Windows路径，后面是linux路径)
-DOWN_URL = r'C:/Users/cong/Desktop/download/pac_excel/' if this_os else r'/opt/pac/download/pac_excel/'
+DOWN_URL = r'C:/Users/static_data/pac_excel/' if this_os else r'/home/pac/static_data/pac_excel/'
 
 # 爬虫爬取的图片及文件保存地址
-WEB_DOWN_FILE_PATH = r'C:/Users/cong/Desktop/download' if this_os else r'/opt/pac/download'
+WEB_DOWN_FILE_PATH = r'C:/Users/static_data' if this_os else r'/home/pac/static_data'
 # 爬虫爬取的图片及文件，如果已有是否重新下载，否则略过
 WEB_DOWN_FILE_REDOWN = False
 
 # 静态文件 static 集成路径
-STATIC_ROOT = r'C:/Users/cong/Desktop/download/static/' if this_os else r'/opt/pac/download/static/'
+STATIC_ROOT = r'C:/Users/static_data/static/' if this_os else r'/home/pac/static_data/static/'
 
 # 谷歌浏览器插件路径 // 找得到？(环境变量)
 EXECUTABLE_PATH = 'chromedriver'
@@ -42,7 +42,7 @@ TESK_THREAD_POOL_EXECUTOR_LEN = 5 if this_os else 10
 DATABASES_ENGINE =  'django.db.backends.mysql'  # 数据库的类型
 DATABASES_NAME =    'pactera'   if this_os else 'pac'              # 所使用的的数据库的名字
 DATABASES_USER =    'root'      if this_os else 'root'             # 数据库服务器的用户
-DATABASES_PASSWORD ='root'      if this_os else '1234567890'      # 密码
-DATABASES_HOST =    '127.0.0.1'if this_os else '211.159.172.213'  # 主机
-DATABASES_PORT =    '3306'      if this_os else '3309'               # 端口
+DATABASES_PASSWORD ='root'      if this_os else 'root'             # 密码
+DATABASES_HOST =    '0-0.cc'    if this_os else 'mysql'            # 主机
+DATABASES_PORT =    '3306'      if this_os else '3306'             # 端口
 
